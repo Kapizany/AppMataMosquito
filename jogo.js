cc2e8aac32f75198b4126ade8345ce7b8b09aa80
Kapizany <kapizany@gmail.com>
2020-02-04 12:54:36 -0300

    Criação do fundo e do Game Over

--- jogo.js
+++ jogo.js
@@ -4,6 +4,24 @@
     let altura = window.innerHeight
     let largura = window.innerWidth
 
+    this.vidas = 3
+
+    this.getVidas = function(){
+        return this.vidas
+    }
+
+    this.setVidas = function( vidas){
+        this.vidas = vidas
+    }
+
+    this.perderVida = function( ){
+        this.setVidas(this.getVidas() - 1)
+    }
+
+    this.ganharVida = function( ){
+        this.setVidas(this.getVidas() + 1)
+    }
+    
     this.ajustaTamanho = function () {
         altura = window.innerHeight
         largura = window.innerWidth
@@ -23,17 +41,54 @@ let janela = new Janela()
 
 
 
-class Mosquito {
-    constructor (){
+function Mosquito (){
+
+    //Remover mosquito caso exista
+    if (!janela.getVidas()){
+        document.getElementById('mosquito').remove()
+            window.location.href = 'fim_de_jogo.html'
+    }
+    else{
+        if (document.getElementById('mosquito')){
+
+            document.getElementById('mosquito').remove()
+            document.getElementById('v'+ janela.getVidas()).src='img/coracao_vazio.png'
+            janela.perderVida()
+        }
+
         this.posicaoX = Math.floor(Math.random()*(janela.getLargura() - 90)) 
         this.posicaoY = Math.floor(Math.random()*(janela.getAltura() - 90))
+
+        this.getClassMosquito = function (){
+            switch ( Math.floor(Math.random()*3)){
+                case 0:
+                    return 'mosquito1'
+                case 1:
+                    return 'mosquito2'
+                case 2:
+                    return 'mosquito3'
+            }
+        }
+
+        this.getLado = function (){
+            switch ( Math.floor(Math.random()*2)){
+                case 0:
+                    return 'ladoA'
+                case 1:
+                    return 'ladoB'
+            }
+        }
         
         this.element = document.createElement('img')
         this.element.src = 'img/mosca.png'
-        this.element.className = 'mosquito1'
+        this.element.id = 'mosquito'
+        this.element.className = this.getClassMosquito() + ' ' + this.getLado()
         this.element.style.left = this.posicaoX + 'px'
         this.element.style.top = this.posicaoY + 'px'
         this.element.style.position = 'absolute'
+        this.element.onclick = function(){
+            this.remove()
+        }
         document.body.appendChild(this.element)
     }
 }
